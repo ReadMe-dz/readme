c3b40455d0e408ee0c798b543f2ff5903312e4d9
AM-77 <amine.griche77@gmail.com>
2020-01-08 21:22:04 +0100

    added the uploads middleware

--- api/routes/books.routes.js
+++ api/routes/books.routes.js
@@ -3,6 +3,7 @@ const books_router = express.Router()
 
 const token_verification = require("../middleware/token-verification")
 const books_controller = require("../controllers/books.controller")
+const uploads = require("../middleware/images-upload")
 
 books_router.get("/search", books_controller.search_books)
 
@@ -10,9 +11,9 @@ books_router.get("/", books_controller.get_all_books)
 
 books_router.get("/:id", books_controller.get_book_by_id)
 
-books_router.post("/", token_verification, books_controller.add_book)
+books_router.post("/", token_verification, uploads.single("picture"), books_controller.add_book)
 
-books_router.patch("/:id", token_verification, books_controller.update_book)
+books_router.patch("/:id", token_verification, uploads.single("picture"), books_controller.update_book)
 
 books_router.delete("/:id", token_verification, books_controller.delete_book)
 
