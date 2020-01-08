f3ba51bf4de0083e04b36ed67196b2e8fbc1ef30
AM-77 <amine.griche77@gmail.com>
2020-01-08 21:18:15 +0100

    renamed the cover property

--- api/models/book.model.js
+++ api/models/book.model.js
@@ -5,7 +5,7 @@ const bookSchema = mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     title: { type: String, required: true },
     author: { type: String, required: true },
-    images: { type: Array },
+    conver: { type: Array },
     price: { type: Number }
 })
 
