56432e627d106d6fcb3172b73befdbc852cb0df5
AM-77 <amine.griche77@gmail.com>
2020-01-08 21:22:52 +0100

    added the images uploading middleware

---
+++ api/middleware/images-upload.js
@@ -0,0 +1,24 @@
+const multer = require("multer")
+
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        if (req.baseUrl === '/users') callback(null, "api/uploads/users")
+        if (req.baseUrl === '/books') callback(null, "api/uploads/books")
+    },
+    filename: (req, file, callback) => {
+        let name = (new Date().getTime()) + (Math.random().toFixed(3) * 1000)
+        let ext = file.originalname.slice(-5)
+        if (ext === ".jpeg") name = name + ext
+        else name = name + ext.slice(-4)
+        callback(null, name)
+    }
+})
+
+const file_filter = (req, file, callback) => {
+    if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") callback(null, true)
+    else callback(new Error("Error! Uncceptable file type."), false)
+}
+
+const uploads = multer({ storage, fileFilter: file_filter, limits: { fileSize: 2097152 } })
+
+module.exports = uploads
