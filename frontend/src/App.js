0ad40e21aaa4d5c6416139bc35440d2a89c4dd57
AM-77 <amine.griche77@gmail.com>
2020-01-10 22:04:24 +0100

    added Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { BrowserRouter as Router, Redirect, Switch, Route } from 'react-router-dom'
+
 import Home from './components/Home'
+import Signin from './components/Signin'
+import Signup from './components/Signup'
 
 export default function App() {
     return (
-        <div className="app">
-            <Home />
-        </div>
+        <Router>
+            <Switch>
+                <Route path="/signin" component={Signin} />
+                <Route path="/signup" component={Signup} />
+                <Route exact={true} path="/" component={Home} />
+                <Redirect to="/" />
+            </Switch>
+        </Router>
     )
 }
