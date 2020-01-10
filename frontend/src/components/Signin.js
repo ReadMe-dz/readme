29fbc399b2ee15b19b05e54d116faf96ce8fecd6
AM-77 <amine.griche77@gmail.com>
2020-01-10 22:03:41 +0100

    added the Singin component

---
+++ frontend/src/components/Signin.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+export default function Signin() {
+
+
+
+    return (
+        <div className="signin">
+            <Navbar />
+
+        </div>
+    )
+}
