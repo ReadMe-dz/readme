7de4c0e2e2fce2adfde6a4b12763edbd50ba3274
AM-77 <amine.griche77@gmail.com>
2020-01-11 16:53:22 +0100

    changed the KEY

--- backend/api/middleware/token-verification.js
+++ backend/api/middleware/token-verification.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken")
 module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(" ")[1]
-        req.verified_token = jwt.verify(token, process.env.JWT_KEY || "What The Fuck")
+        req.verified_token = jwt.verify(token, process.env.JWT_KEY || "G0-p2^vPj1/6$vE[aK1vM3$5")
 
         next()
     } catch (error) {
