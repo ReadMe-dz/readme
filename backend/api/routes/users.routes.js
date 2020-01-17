1b050d0d6b4742a7024cc94eb38d868d5f6b60a8
AM-77 <amine.griche77@gmail.com>
2020-01-17 17:39:06 +0100

    added load_user

--- backend/api/routes/users.routes.js
+++ backend/api/routes/users.routes.js
@@ -2,21 +2,21 @@ const express = require("express")
 const users_router = express.Router()
 
 const token_verification = require("../middleware/token-verification")
-const users_controller = require("../controllers/users.controller")
+const { search_users, get_user_by_id, add_user, update_user, delete_user, login_user, load_user } = require("../controllers/users.controller")
 const uploads = require("../middleware/images-upload")
 
-users_router.get("/search", users_controller.search_users)
+users_router.get("/search", search_users)
 
-users_router.get("/", users_controller.get_all_users)
+users_router.get("/:id", get_user_by_id)
 
-users_router.get("/:id", users_controller.get_user_by_id)
+users_router.post("/", uploads.single("picture"), add_user)
 
-users_router.post("/", uploads.single("picture"), users_controller.add_user)
+users_router.patch("/:id", token_verification, uploads.single("picture"), update_user)
 
-users_router.patch("/:id", token_verification, uploads.single("picture"), users_controller.update_user)
+users_router.delete("/:id", token_verification, delete_user)
 
-users_router.delete("/:id", token_verification, users_controller.delete_user)
+users_router.post("/login", login_user)
 
-users_router.post("/login", users_controller.login_user)
+users_router.get("/", token_verification, load_user)
 
 module.exports = users_router
