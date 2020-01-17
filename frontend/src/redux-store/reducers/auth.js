ae84140dd1759ca73b0a6ec61a7a948695f86b08
AM-77 <amine.griche77@gmail.com>
2020-01-17 17:39:48 +0100

    added load_user & signout

--- frontend/src/redux-store/reducers/auth.js
+++ frontend/src/redux-store/reducers/auth.js
@@ -8,6 +8,19 @@ const auth = (store = {}, action) => {
             user: action.payload.user
         }
 
+        case Types.SIGN_OUT:
+            return {
+                ...store,
+                token: null,
+                user: null
+            }
+
+        case Types.LOAD_USER: return {
+            ...store,
+            token: action.payload.token,
+            user: action.payload.user
+        }
+
         default: return {}
     }
 }
