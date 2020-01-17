d97b4cb17dc46684ff861bc09be9d674b400b6b7
AM-77 <amine.griche77@gmail.com>
2020-01-17 18:09:02 +0100

    added profile when user is logged

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-
+import { isEmptyObj } from "../helpers/functions"
 import searchIcon from '../assets/images/search-outline.svg'
+import signOutIcon from '../assets/images/log-out-outline.svg'
 
-export default function Navbar() {
+export default function Navbar({ sign_out, user }) {
 
     const onInputChange = e => {
         console.log(e.target.value)
@@ -21,12 +22,29 @@ export default function Navbar() {
                 <li className="links">
                     <ul>
                         <li className="search"><div className="icon"><img src={searchIcon} alt="search" /></div><input name="search" type="text" placeholder="Find your next book" onChange={onInputChange} /></li>
+                        {
 
-                        <li className="link"><NavLink to="/signin" activeClassName="active-link">Sign In</NavLink></li>
-                        <li className="link"><NavLink to="/signup" activeClassName="active-link">Sign Up</NavLink></li>
+                            (!isEmptyObj(user))
+                                ?
+                                <div className="profile">
+                                    <NavLink to="/">
+                                        <div className="picture">
+                                            <img src={`http://localhost:3300/${user.picture}`} alt="User profile" />
+                                        </div>
+                                        <p>{user.username}</p>
+                                    </NavLink>
+                                    <button onClick={sign_out}><img src={signOutIcon} alt="logout" /></button>
+                                </div>
+                                :
+                                <div className="singing">
+                                    <li className="link"><NavLink to="/signin" activeClassName="active-link">Sign In</NavLink></li>
+                                    <li className="link"><NavLink to="/signup" activeClassName="active-link">Sign Up</NavLink></li>
+                                </div>
+                        }
                     </ul>
                 </li>
             </ul>
         </div>
     )
 }
+
