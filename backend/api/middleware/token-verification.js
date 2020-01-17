306f2a7be67a12efc2e9f2cab94c9039b4a178ed
AM-77 <amine.griche77@gmail.com>
2020-01-17 17:38:28 +0100

    added token expairation test

--- backend/api/middleware/token-verification.js
+++ backend/api/middleware/token-verification.js
@@ -2,10 +2,17 @@ const jwt = require("jsonwebtoken")
 
 module.exports = (req, res, next) => {
     try {
-        const token = req.headers.authorization.split(" ")[1]
-        req.verified_token = jwt.verify(token, process.env.JWT_KEY || "G0-p2^vPj1/6$vE[aK1vM3$5")
-
-        next()
+        if (req.headers && req.headers.authorization) {
+            const token = req.headers.authorization.split(" ")[1]
+            if (jwt.decode(token).exp > (Date.now() / 1000)) {
+                req.verified_token = jwt.verify(token, process.env.JWT_KEY || "G0-p2^vPj1/6$vE[aK1vM3$5")
+                next()
+            } else {
+                res.status(401).json({ message: "Authentication Failed.", error: "Token Expaired." })
+            }
+        } else {
+            res.status(401).json({ message: "Authentication Failed.", error: "Unvalid Token." })
+        }
     } catch (error) {
         res.status(401).json({ message: "Auth Failed." })
     }
