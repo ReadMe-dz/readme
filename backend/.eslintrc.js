78334f567f0a6c3532206f44a42b583b10a9498c
AM-77 <amine.griche77@protonmail.ch>
2020-11-03 07:48:11 +0100

    add configs

---
+++ backend/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    jest: true,
+    node: true,
+  },
+  extends: ["airbnb-base", "plugin:prettier/recommended"],
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    "no-underscore-dangle": "off",
+  },
+};
