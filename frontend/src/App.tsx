a1699d8d5b7c86058c1fc1239d4fae6f7690b0bb
AM-77 <amine.griche77@protonmail.ch>
2020-11-05 08:35:00 +0100

    add router, some routes & redux store provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Redirect, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux-store';
+import CheckAuthentication from './utils/CheckAuthentication';
+import GuestRoute from './utils/GeustRoute';
+import PrivateRoute from './utils/PrivateRouter';
+import Home from './pages/Home';
+import Logout from './pages/Logout';
+import Login from './pages/Login';
+import Subscribe from './pages/Subscribe';
 
-function App() {
+const App: React.FC = () => {
+  useEffect(() => {
+    CheckAuthentication();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          <PrivateRoute exact path="/" component={Home} />
+          <GuestRoute path="/login" component={Login} />
+          <GuestRoute path="/subscribe" component={Subscribe} />
+          <PrivateRoute exact path="/logout" component={Logout} />
+          <Redirect to="/" />
+        </Switch>
+      </Router>
+    </Provider>
   );
-}
+};
 
 export default App;
