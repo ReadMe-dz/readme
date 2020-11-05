f8d3c91e624d13b227723c10133155e034dac10d
AM-77 <amine.griche77@protonmail.ch>
2020-11-05 19:36:33 +0100

    update user validators

--- frontend/src/validations/user.validator.ts
+++ frontend/src/validations/user.validator.ts
@@ -1,23 +1,47 @@
 import * as Yup from 'yup';
+import wilayas from '../utils/data/wilayas.json';
 
-const firstName: Yup.StringSchema = Yup.string()
-  .min(4, 'Must be 4 characters or more')
-  .max(15, 'Must be 15 characters or less')
+const name: Yup.StringSchema = Yup.string()
+  .matches(
+    // eslint-disable-next-line
+    new RegExp(/([a-zA-Z]{3,45}\s*)+/),
+    'Invalid name, only [a-z] characters are allowed'
+  )
+  .min(3, 'Must be 3 characters or more')
+  .max(45, 'Must be 45 characters or less')
   .required('Required');
-const lastName: Yup.StringSchema = Yup.string()
-  .min(4, 'Must be 4 characters or more')
+
+const username: Yup.StringSchema = Yup.string()
+  .matches(
+    // eslint-disable-next-line
+    new RegExp(/^[a-zA-Z0-9_]+$/),
+    'Invalid username'
+  )
+  .min(3, 'Must be 4 characters or more')
   .max(20, 'Must be 20 characters or less')
   .required('Required');
-const userName: Yup.StringSchema = Yup.string()
-  .min(4, 'Must be 4 characters or more')
-  .max(20, 'Must be 20 characters or less')
-  .required('Required');
+
 const password: Yup.StringSchema = Yup.string()
   .min(8, 'Must be 8 characters or more')
   .max(20, 'Must be 20 characters or less')
   .required('Required');
+
 const email: Yup.StringSchema = Yup.string()
+  .matches(
+    // eslint-disable-next-line
+    new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/),
+    'Invalid email address'
+  )
   .email('Invalid email address')
   .required('Required');
 
-export default { firstName, lastName, userName, password, email };
+const wilaya: Yup.StringSchema = Yup.string()
+  .oneOf(wilayas, 'Invalid wilaya name')
+  .required('Required');
+
+const terms: Yup.BooleanSchema = Yup.boolean().oneOf(
+  [true],
+  'Must accept Terms and Conditions'
+);
+
+export default { name, username, password, email, wilaya, terms };
