032d68b7580052ee83b848bd5abc111b8191c115
AM-77 <amine.griche77@protonmail.ch>
2020-11-07 01:05:38 +0100

    update user validator

--- frontend/src/validations/user.validator.ts
+++ frontend/src/validations/user.validator.ts
@@ -4,8 +4,8 @@ import wilayas from '../utils/data/wilayas.json';
 const name: Yup.StringSchema = Yup.string()
   .matches(
     // eslint-disable-next-line
-    new RegExp(/([a-zA-Z]{3,45}\s*)+/),
-    'Invalid name, only [a-z] characters are allowed'
+    new RegExp(/^[a-zA-Z]+$/),
+    'Invalid name, only ( a...z ) characters are allowed'
   )
   .min(3, 'Must be 3 characters or more')
   .max(45, 'Must be 45 characters or less')
@@ -15,7 +15,7 @@ const username: Yup.StringSchema = Yup.string()
   .matches(
     // eslint-disable-next-line
     new RegExp(/^[a-zA-Z0-9_]+$/),
-    'Invalid username'
+    'Invalid username, only ( a...z _ ) characters are allowed'
   )
   .min(3, 'Must be 4 characters or more')
   .max(20, 'Must be 20 characters or less')
