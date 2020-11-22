a02e35ae84e216786d33275e3fa0374ecaa92d06
AM-77 <amine.griche77@protonmail.ch>
2020-11-22 20:44:46 +0100

    update prettier

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,21 +1,21 @@
-const express = require("express");
-const cors = require("cors");
-const path = require("path");
-const connect = require("./db/connect");
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const connect = require('./db/connect');
 
 const app = express();
 connect();
 
-const booksRouter = require("./routes/books.routes");
-const usersRouter = require("./routes/users.routes");
-const errorsRouter = require("./routes/errors.routes");
+const booksRouter = require('./routes/books.routes');
+const usersRouter = require('./routes/users.routes');
+const errorsRouter = require('./routes/errors.routes');
 
-app.use("/api/uploads", express.static(path.join(__dirname, "/uploads")));
+app.use('/api/uploads', express.static(path.join(__dirname, '/uploads')));
 app.use(cors());
 app.use(express.json());
 
-app.use("/books", booksRouter);
-app.use("/users", usersRouter);
+app.use('/books', booksRouter);
+app.use('/users', usersRouter);
 app.use(errorsRouter);
 
 module.exports = app;
