bf0b242715f1dfc9599f9f9d91d449e9d0379523
AM-77 <amine.griche77@protonmail.ch>
2020-11-22 20:41:51 +0100

    add mailer

---
+++ backend/api/utils/mailer.js
@@ -0,0 +1,25 @@
+const nodemailer = require('nodemailer');
+
+const { EMAIL, PASS } = process.env;
+const mailer = async (email, subject, html) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: EMAIL,
+        pass: PASS,
+      },
+    });
+
+    await transporter.sendMail({
+      from: 'Read Me 📚',
+      to: email,
+      subject,
+      html,
+    });
+  } catch (error) {
+    console.log('[!] Error in mailer', error);
+  }
+};
+
+module.exports = mailer;
