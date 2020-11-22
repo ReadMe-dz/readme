03458f738fe0c40658d87d3ea1fa2dda6040c983
AM-77 <amine.griche77@protonmail.ch>
2020-11-22 20:43:51 +0100

    add default cover

---
+++ backend/api/validations/user.validator.js
@@ -0,0 +1,43 @@
+const Joi = require('joi');
+const wilayas = require('../constants/wilayas');
+
+const user = Joi.object({
+  name: Joi.string()
+    .matches(new RegExp(/^[a-zA-Z ]+$/))
+    .min(3)
+    .max(15),
+
+  username: Joi.string()
+    .matches(new RegExp(/^[a-zA-Z0-9_]+$/))
+    .min(3)
+    .max(9),
+
+  password: Joi.string().min(8).max(20),
+
+  birthdate: Joi.number().integer().min(1900).max(2013),
+
+  email: Joi.string().email(),
+
+  phone: Joi.string().matches(
+    // eslint-disable-next-line
+    new RegExp(/^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/)
+  ),
+
+  twitter: Joi.string().matches(
+    // eslint-disable-next-line
+    new RegExp(/http(?:s)?:\/\/(?:www\.)?twitter\.com\/([a-zA-Z0-9_]+)/i)
+  ),
+
+  facebook: Joi.string().matches(
+    new RegExp(
+      // eslint-disable-next-line
+      /(?:https?:\/\/)?(?:www\.)?(mbasic.facebook|m\.facebook|facebook|fb)\.(com|me)\/(?:(?:\w\.)*#!\/)?(?:pages\/)?(?:[\w\-\.]*\/)*([\w\-\.]*)/gi
+    )
+  ),
+
+  wilaya: Joi.valid(...wilayas),
+
+  terms: Joi.boolean().valid(true),
+});
+
+module.exports = user;
