44f384987ea350c322a297be6b405ae0ec508818
AM-77 <amine.griche77@protonmail.ch>
2020-11-23 01:34:11 +0100

    add routes, check-auth, clear-msg, ...

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,18 +2,25 @@ import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Redirect, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { clearMsg } from './redux-store/actions/msg.actions';
+import { selectBook } from './redux-store/actions/book.actions';
 import CheckAuthentication from './utils/CheckAuthentication';
 import GuestRoute from './utils/GeustRoute';
 import PrivateRoute from './utils/PrivateRouter';
+import Message from './components/Message';
 import Home from './pages/Home';
 import Logout from './pages/Logout';
 import Login from './pages/Login';
 import Subscribe from './pages/Subscribe';
+import BookDetails from './containers/BookDetails';
 import ForgetPassword from './pages/ForgetPassword';
-import Message from './components/Message';
 import Profile from './pages/Profile';
+import EditProfile from './pages/EditProfile';
+import AddBook from './pages/AddBook';
+import EditBook from './pages/EditBook';
+import NotFound from './pages/NotFound';
+import ResetPassword from './pages/resetPassword';
 
-const App: React.FC<any> = ({ msg, clear }: any) => {
+const App: React.FC<any> = ({ msg, clear, book, selectedBook }: any) => {
   useEffect(() => {
     CheckAuthentication();
   }, []);
@@ -22,7 +29,7 @@ const App: React.FC<any> = ({ msg, clear }: any) => {
     if (msg.content) {
       setTimeout(() => {
         clear();
-      }, 4000);
+      }, 6000);
     }
   }, [msg]);
 
@@ -30,17 +37,22 @@ const App: React.FC<any> = ({ msg, clear }: any) => {
     <div className="app">
       {msg.content && <Message type={msg.type} content={msg.content} />}
       <Router>
+        {book.id && (
+          <BookDetails id={book.id} closeDetails={() => selectedBook(null)} />
+        )}
         <Switch>
           <PrivateRoute exact path="/" component={Home} />
-          <PrivateRoute
-            path="/profile/:id"
-            render={(routeProps) => <Profile {...routeProps} />}
-          />
+          <PrivateRoute path="/user/:id" component={Profile} />
+          <PrivateRoute path="/edit" component={EditProfile} />
+          <PrivateRoute path="/add-book" component={AddBook} />
+          <PrivateRoute path="/book/:id" component={EditBook} />
           <GuestRoute path="/login" component={Login} />
           <GuestRoute path="/subscribe" component={Subscribe} />
           <GuestRoute path="/forget-password" component={ForgetPassword} />
-          <PrivateRoute exact path="/logout" component={Logout} />
-          <Redirect to="/" />
+          <GuestRoute path="/reset/:resetToken" component={ResetPassword} />
+          <PrivateRoute path="/logout" component={Logout} />
+          <PrivateRoute path="/not-found" component={NotFound} />
+          <Redirect to="/not-found" />
         </Switch>
       </Router>
     </div>
@@ -49,9 +61,11 @@ const App: React.FC<any> = ({ msg, clear }: any) => {
 
 const mapStateToProps = (state: any) => ({
   msg: state.msg,
+  book: state.book,
 });
 
 const mapActionsToProps = {
   clear: clearMsg,
+  selectedBook: selectBook,
 };
 export default connect(mapStateToProps, mapActionsToProps)(App);
