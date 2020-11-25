126e265a622fead8443e2cab6807f5fcb54ba423
AM-77 <amine.griche77@protonmail.ch>
2020-11-25 18:48:12 +0100

    update <Message />

--- frontend/src/components/Message/Message.tsx
+++ frontend/src/components/Message/Message.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { clearMsg } from '../../redux-store/actions/msg.actions';
+import Button from '../Button';
+import getIcon from '../../utils/icons';
 import './style.scss';
 
 type props = {
   content: string;
   type: 'success' | 'error';
+  clear: () => void;
 };
 
-const Message: React.FC<props> = ({ content, type }) => {
+const Message: React.FC<props> = ({ content, type, clear }) => {
   return (
     <div className={`message ${type}`}>
       <p>{content}</p>
+      <Button content={getIcon('close')} onClick={clear} />
     </div>
   );
 };
@@ -18,6 +24,11 @@ const Message: React.FC<props> = ({ content, type }) => {
 Message.propTypes = {
   content: PropTypes.string.isRequired,
   type: PropTypes.oneOf<'success' | 'error'>(['success', 'error']).isRequired,
+  clear: PropTypes.func.isRequired,
 };
 
-export default Message;
+const mapActionsToProps = {
+  clear: clearMsg,
+};
+
+export default connect(null, mapActionsToProps)(Message);
