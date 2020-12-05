0684223457931a62d879b467c029a3b082e50d6c
AM-77 <amine.griche77@protonmail.ch>
2020-12-05 23:07:30 +0100

    add comments controller

---
+++ backend/api/controllers/comments/index.js
@@ -0,0 +1,5 @@
+const commentsWS = require('./commentsWS');
+const postComment = require('./postComment');
+const getCommentsByBookId = require('./getCommentsByBookId');
+
+module.exports = { commentsWS, postComment, getCommentsByBookId };
