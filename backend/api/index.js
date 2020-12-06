217a35bf14a692242dd16405f98494fad2036db6
AM-77 <amine.griche77@protonmail.ch>
2020-12-06 03:33:11 +0100

    replace express-ws with socket.io

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const expressWs = require('express-ws');
 const cors = require('cors');
 const path = require('path');
 const connect = require('./db/connect');
 
 const app = express();
-const ws = expressWs(app);
 connect();
 
 const booksRouter = require('./routes/books.routes');
@@ -13,7 +11,6 @@ const usersRouter = require('./routes/users.routes');
 const reportsRouter = require('./routes/reports.routes');
 const commentsRouter = require('./routes/comments.routes');
 const errorsRouter = require('./routes/errors.routes');
-const { commentsWS } = require('./controllers/comments');
 
 app.use('/api/uploads', express.static(path.join(__dirname, '/uploads')));
 app.use(cors());
@@ -24,7 +21,5 @@ app.use('/users', usersRouter);
 app.use('/reports', reportsRouter);
 app.use('/comments', commentsRouter);
 app.use(errorsRouter);
-
-app.ws('/comments', commentsWS(ws));
 
 module.exports = app;
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 const http = require('http');
+const socket = require('socket.io');
 const api = require('./api');
+const { WSComments } = require('./api/controllers/comments');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3300;
 const server = http.createServer(api);
+WSComments(socket(server));
 
 server.listen(PORT, () => console.log(`[+] server running on port: ${PORT}`));
