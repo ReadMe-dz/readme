3f0832f8abe21adb293522f4358468de617f8c2a
AM-77 <amine.griche77@protonmail.ch>
2020-12-07 23:02:06 +0100

    add WSMessaegs

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,13 @@ const http = require('http');
 const socket = require('socket.io');
 const api = require('./api');
 const { WSComments } = require('./api/controllers/comments');
+const { WSMessages } = require('./api/controllers/messages');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3300;
 const server = http.createServer(api);
-WSComments(socket(server));
+const io = socket(server);
+WSComments(io);
+WSMessages(io);
 
 server.listen(PORT, () => console.log(`[+] server running on port: ${PORT}`));
