353c6fb2cc9ac91ba265d0a34e27eaacc980cd38
amine griche <amine.griche77@protonmail.ch>
2024-02-04 20:55:29 +0100

    feat(backend): update the db connection

--- backend/api/db/connect.js
+++ backend/api/db/connect.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose');
 
 module.exports = () => {
-  const DB_URL = process.env.DB_URL || 'localhost';
-  const DB_PORT = process.env.DB_PORT || 27017;
-  const DB_NAME = process.env.DB_NAME || 'books';
+  const MONGODB_URL = process.env.MONGODB_URL;
 
   mongoose.connect(
-    `mongodb://${DB_URL}:${DB_PORT}/${DB_NAME}`,
+    MONGODB_URL,
     {
       useUnifiedTopology: true,
       useNewUrlParser: true,
